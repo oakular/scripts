6b3e0e327ed5c538bceff44a69c73e025bcbb196
oakular <callum@oakular.com>
2016-12-01 10:38:56 +0000

    put code into functions

--- gradeCalc.py
+++ gradeCalc.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python
-# take user input for number of assignments
-# they have completed so far
-assignmNumInput = input("enter the number of assignments completed: ")
-assignmNum = int(assignmNumInput)
 
-for count in range(1, assignmNum):
-    assignmGrades[0] = input("enter the grade as a percent for assignment "
-                       + str(count))
+def getAssignmGrades():
+    # ---- for loop to take percentages achieved for
+    # each assignment
+    for count in range(1, assignmNum):
+        assignmGrades[0] = input("enter the grade as a percent for assignment "
+                + str(count) + ": ")
+return
+
+def getAssignmNum():
+    # take user input for number of assignments
+    # they have completed so far
+    assignmNumInput = input("enter the number of assignments completed: ")
+    assignmNum = int(assignmNumInput)
+
+    assignmGrades = [assignmNum]
+return
