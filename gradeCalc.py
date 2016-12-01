9c0d57d95846a587bc172446ca0274c65f64c686
oakular <callum@oakular.com>
2016-12-01 13:24:53 +0000

    program takes percentage grades and weighting

--- gradeCalc.py
+++ gradeCalc.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
 
-def getAssignmGrades():
+def getAssignmGrade(assignmGrade, assignmWeight, assignmNum):
     # ---- for loop to take percentages achieved for
     # each assignment
-    for count in range(1, assignmNum):
-        assignmGrades[0] = input("enter the grade as a percent for assignment "
-                + str(count) + ": ")
-return
+    for count in range(0, assignmNum):
+        assignmGrade.append(input("enter the grade as a percent for assignment "
+                + str(count) + ": "))
+        assignmWeight.append(input("enter the weighting for the above"
+                + "assignment: "))
+    return
 
+# ---- function to ask user for the number of assignments they have
+# completed for the module
 def getAssignmNum():
     # take user input for number of assignments
     # they have completed so far
     assignmNumInput = input("enter the number of assignments completed: ")
     assignmNum = int(assignmNumInput)
 
-    assignmGrades = [assignmNum]
-return
+    assignmGrade = [assignmNum]
+    assignmWeight = [assignmNum]
+
+    getAssignmGrade(assignmGrade, assignmWeight, assignmNum)
+    return
+
+def divideByOneHundred(weight):
+    return grade / 100
+
+# start the program
+getAssignmNum()
